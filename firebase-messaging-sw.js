429243517405c18b8e00d1c494effd02bd46fe1a
nosiemaa <kacper17lol@gmail.com>
2025-11-25 23:19:31 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD7z5z5z5z5z5z5z5z5z5z5z5z5z5z5z5z5",
+  apiKey: "AIzaSyD7z5z5z5z5z5z5z5z5z5z5z5z5z5z5z5",
   authDomain: "odliczanie-2026.firebaseapp.com",
   projectId: "odliczanie-2026",
   messagingSenderId: "103953800507",
@@ -10,3 +10,23 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = payload.notification?.title || 'Odliczanie zakończone!';
+  const notificationOptions = {
+    body: payload.notification?.body || 'Twoje odliczanie właśnie się skończyło!',
+    icon: '/zegar-192x192.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
