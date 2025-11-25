0a6f997fa7a7a91c9f8dea8218f7b61c66b800db
nosiemaa <kacper17lol@gmail.com>
2025-11-25 22:09:55 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+<link rel="manifest" href="/manifest.json">
+<link rel="apple-touch-icon" href="/zegar-192.png">
+<meta name="theme-color" content="#00ff00">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
