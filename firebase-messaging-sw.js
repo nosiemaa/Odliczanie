16fab93f49ce13e07563b8a32f4c9557b924725b
nosiemaa <kacper17lol@gmail.com>
2025-11-25 22:49:20 +0100

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD7z5z5z5z5z5z5z5z5z5z5z5z5z5z5z5z5",
+  authDomain: "odliczanie-2026.firebaseapp.com",
+  projectId: "odliczanie-2026",
+  messagingSenderId: "103953800507",
+  appId: "1:103953800507:web:123456789"
+});
+
+const messaging = firebase.messaging();
