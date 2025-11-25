1dfb688587b9581eb68427d931efe15ec3d6fb62
nosiemaa <kacper17lol@gmail.com>
2025-11-25 22:54:00 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,20 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD7z5z5z5z5z5z5z5z5z5z5z5z5z5z5z5z5",
+  apiKey: "AIzaSyDXXXXXXX",          // Twój z ekranu
   authDomain: "odliczanie-2026.firebaseapp.com",
   projectId: "odliczanie-2026",
-  messagingSenderId: "103953800507",
-  appId: "1:103953800507:web:123456789"
+  messagingSenderId: "1039538080607",
+  appId: "1:1039538080607:web:1123456789"
 });
 
 const messaging = firebase.messaging();
+
+// Powiadomienia w tle
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png"
+  });
+});
